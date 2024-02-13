d9cbcbaf539857011bdbec9e58de20bb793b4bef
Mudaasirl <shabanab488@gmail.com>
2024-02-13 15:36:15 +0530

    changed home page sign in

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Post,User
-from users.models import Profile
+from django.shortcuts import redirect, render
+from .models import Post
 
-def home(request):
-    context={
-        'pass':'pass'
-    }
-    return render(request,'blog/home.html',context)
+def home(request):   
+    return redirect('sign_in')
+
+#shall be removed in future
 def index(request):
     context = {
         'posts': Post.objects.all(),
     }
     return render(request, 'blog/index.html', context)
-def get_all_users(request):
-    context = {
-        'users' : User.objects.all()
-    }
-  
-    return render(request, 'blog/users.html', context)
-def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
-def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,23 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from users import views as user_views
 from blog import views as blog_views
-from django.contrib.auth import views as auth_views
 from django.conf import settings
-from views import Home # new
 from storynator.views import sign_out,AuthGoogle
 from django.conf.urls.static import static
+
 urlpatterns = [
-    path('',blog_views.home,name='home'),
     path('admin/', admin.site.urls),
+    path('',blog_views.home,name='home'),
     path('blog/', include("blog.urls")),
-    path('storynator/',include("storynator.urls")),
-    path('register/', user_views.register, name ='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile/', user_views.profile, name='profile'),
-    path('accounts/', include('allauth.urls')), # new
-    path('register', Home.as_view(), name='home'), # new
+    path('storynator/',include("storynator.urls")),    
     path('sign-out', sign_out, name='sign_out'),
     path('auth-receiver', AuthGoogle.as_view(), name='auth_receiver'),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
